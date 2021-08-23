c285d629084eb08b88c97ba7b51f35baff25ed46
YamagishiRyu <bubusu.ryu@gmail.com>
2021-08-24 08:01:56 +0900

    Add: close discussion

--- repo/discussion.go
+++ repo/discussion.go
@@ -29,6 +29,7 @@ func NewDiscussionRepository(githubClient *githubv4.Client) DiscussionRepository
 
 type (
 	Discussion struct {
+		Id       githubv4.ID
 		Body     githubv4.String
 		Url      githubv4.String
 		Title    githubv4.String
@@ -45,7 +46,6 @@ type (
 )
 
 func (r *discussionRepositoryImpl) Create(ctx context.Context, issue types.Issue) (types.Issue, error) {
-
 	return types.Issue{}, nil
 }
 
@@ -114,10 +114,10 @@ func (r *discussionRepositoryImpl) FindLastIssueByLabel(ctx context.Context, iss
 	if err != nil {
 		return types.Issue{}, errors.WithStack(err)
 	}
-
 	if len(q.Search.Nodes) == 0 {
 		return issue, errors.New("Not found last issue")
 	}
+
 	lastDiscussion := q.Search.Nodes[0].Discussion
 	for _, node := range q.Search.Nodes {
 		if lastDiscussion.CreatedAt.Time.Before(node.Discussion.CreatedAt.Time) {
@@ -125,6 +125,7 @@ func (r *discussionRepositoryImpl) FindLastIssueByLabel(ctx context.Context, iss
 			lastDiscussion = node.Discussion
 		}
 	}
+
 	ls := make([]string, 0, len(lastDiscussion.Labels.Nodes))
 	for _, label := range lastDiscussion.Labels.Nodes {
 		ls = append(ls, string(label.Name))
@@ -141,5 +142,63 @@ func (r *discussionRepositoryImpl) FindLastIssueByLabel(ctx context.Context, iss
 }
 
 func (r *discussionRepositoryImpl) CloseByURL(ctx context.Context, issueURL string) (types.Issue, error) {
+	discussionData, err := parseIssueURL(issueURL)
+	if err != nil {
+		return types.Issue{}, errors.WithStack(err)
+	}
+
+	var q struct {
+		Repository struct {
+			Id                 githubv4.String
+			DiscussionCategory struct {
+				Nodes []struct {
+					Id   githubv4.ID
+					Name githubv4.String
+				}
+			} `graphql:"discussionCategories(first: 100)"`
+			Discussion `graphql:"discussion(number: $number)"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
+	}
+
+	variables := map[string]interface{}{
+		"number": githubv4.Int(discussionData.IssueNumber),
+		"owner":  githubv4.String(discussionData.Owner),
+		"name":   githubv4.String(discussionData.Repository),
+	}
+
+	err = r.ghc.Query(ctx, &q, variables)
+	if err != nil {
+		return types.Issue{}, errors.WithStack(err)
+	}
+
+	var archiveCategoryId githubv4.ID
+	for _, category := range q.Repository.DiscussionCategory.Nodes {
+		if category.Name == "Archive" {
+			archiveCategoryId = category.Id
+			break
+		}
+	}
+	if archiveCategoryId == nil {
+		return types.Issue{}, errors.New("Archive Category Not Found")
+	}
+
+	var m struct {
+		UpdateDiscussion struct {
+			Discussion
+		}
+	}
+
+	input := githubv4.UpdateDiscussionInput{
+		DiscussionID: q.Repository.Discussion.Id,
+		Title:        &q.Repository.Discussion.Title,
+		Body:         &q.Repository.Discussion.Body,
+		CategoryID:   &archiveCategoryId,
+	}
+
+	err = r.ghc.Mutate(ctx, &m, input, nil)
+	if err != nil {
+		return types.Issue{}, errors.WithStack(err)
+	}
+
 	return types.Issue{}, nil
 }
