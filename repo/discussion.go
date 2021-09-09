4cc1b02b2399280c1d4f27502740e33d08f059e8
YamagishiRyu <bubusu.ryu@gmail.com>
2021-09-09 19:28:07 +0900

    Fix: variable case

--- repo/discussion.go
+++ repo/discussion.go
@@ -19,7 +19,7 @@ type discussionRepositoryImpl struct {
 }
 
 type (
-	discussion struct {
+	Discussion struct {
 		Id       githubv4.ID
 		Body     githubv4.String
 		Url      githubv4.String
@@ -57,7 +57,7 @@ func (r *discussionRepositoryImpl) Create(ctx context.Context, issue types.Issue
 	var m struct {
 		CreateDiscussion struct {
 			Discussion struct {
-				discussion
+				Discussion
 			}
 		} `graphql:"createDiscussion(input: $input)"`
 	}
@@ -73,7 +73,7 @@ func (r *discussionRepositoryImpl) Create(ctx context.Context, issue types.Issue
 		return types.Issue{}, errors.WithStack(err)
 	}
 
-	d := m.CreateDiscussion.Discussion.discussion
+	d := m.CreateDiscussion.Discussion.Discussion
 	meta := map[string]string{
 		categoryKey: fmt.Sprintf("%+v", d.Category.Id),
 	}
@@ -96,7 +96,7 @@ func (r *discussionRepositoryImpl) FindByURL(ctx context.Context, issueURL strin
 	var q struct {
 		Repository struct {
 			Id         githubv4.String
-			discussion `graphql:"discussion(number: $number)"`
+			Discussion `graphql:"discussion(number: $number)"`
 		} `graphql:"repository(owner: $owner, name: $name)"`
 	}
 
@@ -112,16 +112,16 @@ func (r *discussionRepositoryImpl) FindByURL(ctx context.Context, issueURL strin
 	}
 
 	meta := map[string]string{
-		categoryKey: fmt.Sprintf("%+v", q.Repository.discussion.Category.Id),
+		categoryKey: fmt.Sprintf("%+v", q.Repository.Discussion.Category.Id),
 	}
 
 	return types.Issue{
 		Owner:      discussionData.Owner,
 		Repository: discussionData.Repository,
 
-		Title: string(q.Repository.discussion.Title),
-		Body:  string(q.Repository.discussion.Body),
-		URL:   (*string)(&q.Repository.discussion.Url),
+		Title: string(q.Repository.Discussion.Title),
+		Body:  string(q.Repository.Discussion.Body),
+		URL:   (*string)(&q.Repository.Discussion.Url),
 		Meta:  &meta,
 	}, nil
 }
@@ -130,7 +130,7 @@ func (r *discussionRepositoryImpl) FindLastIssue(ctx context.Context, templateIs
 	var q struct {
 		Search struct {
 			Nodes []struct {
-				discussion `graphql:"... on Discussion"`
+				Discussion `graphql:"... on Discussion"`
 			}
 		} `graphql:"search(query: $query, type: $type, first: 100)"`
 	}
@@ -155,11 +155,11 @@ func (r *discussionRepositoryImpl) FindLastIssue(ctx context.Context, templateIs
 		return templateIssue, errors.New(LastDiscussionNotFound)
 	}
 
-	lastDiscussion := q.Search.Nodes[0].discussion
+	lastDiscussion := q.Search.Nodes[0].Discussion
 	for _, node := range q.Search.Nodes {
-		if lastDiscussion.CreatedAt.Time.Before(node.discussion.CreatedAt.Time) {
+		if lastDiscussion.CreatedAt.Time.Before(node.Discussion.CreatedAt.Time) {
 			// when finding more recent discussion
-			lastDiscussion = node.discussion
+			lastDiscussion = node.Discussion
 		}
 	}
 
@@ -191,7 +191,7 @@ func (r *discussionRepositoryImpl) CloseByURL(ctx context.Context, issueURL stri
 					Name githubv4.String
 				}
 			} `graphql:"discussionCategories(first: 100)"`
-			discussion `graphql:"discussion(number: $number)"`
+			Discussion `graphql:"discussion(number: $number)"`
 		} `graphql:"repository(owner: $owner, name: $name)"`
 	}
 
@@ -220,15 +220,15 @@ func (r *discussionRepositoryImpl) CloseByURL(ctx context.Context, issueURL stri
 	var m struct {
 		UpdateDiscussion struct {
 			Discussion struct {
-				discussion
+				Discussion
 			}
 		} `graphql:"updateDiscussion(input: $input)"`
 	}
 
 	input := githubv4.UpdateDiscussionInput{
-		DiscussionID: q.Repository.discussion.Id,
-		Title:        &q.Repository.discussion.Title,
-		Body:         &q.Repository.discussion.Body,
+		DiscussionID: q.Repository.Discussion.Id,
+		Title:        &q.Repository.Discussion.Title,
+		Body:         &q.Repository.Discussion.Body,
 		CategoryID:   &archiveCategoryId,
 	}
 
@@ -237,7 +237,7 @@ func (r *discussionRepositoryImpl) CloseByURL(ctx context.Context, issueURL stri
 		return types.Issue{}, errors.WithStack(err)
 	}
 
-	d := m.UpdateDiscussion.Discussion.discussion
+	d := m.UpdateDiscussion.Discussion.Discussion
 	meta := map[string]string{
 		categoryKey: fmt.Sprintf("%+v", d.Category.Id),
 	}
