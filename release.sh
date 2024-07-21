4812bdf67c80b8a8cbfcac50f9acdfc89ce06cb7
Hazumi Ichijo <hahihu314+github@gmail.com>
2024-07-22 02:38:32 +0900

    Commit to master

--- release.sh
+++ release.sh
@@ -8,28 +8,17 @@ if [ $# -ne 1 ]; then
 fi
 
 tag=$1
-current_commit=$(git rev-parse HEAD)
 
-# Actions が利用できるDocker Imageの制限に対応するため。 https://stackoverflow.com/questions/76403845/when-accessing-github-marketplace-actions-i-am-seeing-getting-error-should-be-e 
 echo "tag: $tag"
-echo "current_commit: $current_commit"
-
-# Create working branch
-git checkout master
-git pull origin master
-git checkout -b releaser/$current_commit
 
 # Create Dockerfile
+# Actions が利用できるDocker Imageの制限に対応するため。 https://stackoverflow.com/questions/76403845/when-accessing-github-marketplace-actions-i-am-seeing-getting-error-should-be-e 
 echo "FROM ghcr.io/rerost/issue-creator:${tag}" > Dockerfile.actions
 git add Dockerfile.actions
 git commit -m "Generate docker file"
+git push origin master
 
 # Create Tag
 git tag -a $tag -m "$tag"
 git push origin $tag
 echo "RELASED"
-
-# Clean
-git checkout master
-git branch -D releaser/$current_commit
-echo "CLEANED"
